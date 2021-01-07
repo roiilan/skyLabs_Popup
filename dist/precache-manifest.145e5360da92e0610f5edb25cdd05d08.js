3fddf98b7b5c8702504fa0f443629a412c3863db
roi.ilan <roiilan23@gmail.com>
2021-01-07 13:37:32 +0200

    Initial dist subtree commit

---
+++ dist/precache-manifest.145e5360da92e0610f5edb25cdd05d08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e09299ef340d6363f94",
+    "url": "/skyLabs_Popup/css/app.31195f24.css"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/skyLabs_Popup/fonts/Open-Sans.629a55a7.ttf"
+  },
+  {
+    "revision": "c1678b46f7dd3f50ceac94ed4e0ad01a",
+    "url": "/skyLabs_Popup/fonts/SourceSansPro-Regular.c1678b46.ttf"
+  },
+  {
+    "revision": "848c9fe35c46830700a8d05817e7e5f0",
+    "url": "/skyLabs_Popup/index.html"
+  },
+  {
+    "revision": "1e09299ef340d6363f94",
+    "url": "/skyLabs_Popup/js/app.f6cbb661.js"
+  },
+  {
+    "revision": "24fdcbbede81c0adf7a2",
+    "url": "/skyLabs_Popup/js/chunk-vendors.3aca0e4c.js"
+  },
+  {
+    "revision": "b800a5bbe0cae45d953e237bb1d52b6b",
+    "url": "/skyLabs_Popup/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/skyLabs_Popup/robots.txt"
+  }
+]);
