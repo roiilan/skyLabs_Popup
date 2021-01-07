8862b5aa42151f32a497909c90968b213d9f5a45
roi.ilan <roiilan23@gmail.com>
2021-01-07 12:51:36 +0200

    upload the project

---
+++ src/components/popup.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <button class="pointer" @click="$emit('togglePopup')">click me to open the popup</button>
+
+    <section class="popup-container" :class="{ active: isActive }">
+      <span class="close-btn" @click="$emit('togglePopup')">
+        <img src="@/assets/popup_img/X.jpg" />
+      </span>
+      <span class="main-title">
+        <img src="@/assets/popup_img/lemonade.png" alt="" />
+        The Lemonade IPO takes place today
+      </span>
+      <span class="sub-title">Will we see a surge or a crash?</span>
+      <span class="btn">
+        <span class="desktop">Find out more</span><span class="mobile">Act now</span>
+      </span>
+    </section>
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: "popup",
+  props: {
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <div @click="isActive = false" class="backdrop" :class="{ active: isActive }"></div>
+    <popup class="popup" :isActive="isActive" @togglePopup="isActive = !isActive" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import popup from "@/components/popup.vue";
+
+export default {
+  name: "Home",
+  components: {
+    popup,
+  },
+  data() {
+    return {
+      isActive: true,
+    };
+  },
+};
+</script>
